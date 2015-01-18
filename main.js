23c1ee32b7fd38aa04bf8443e59dd00792e9991f
Patrick Werner <pwerner2@gmail.com>
2015-01-17 19:25:40 -0500

    changed button class names, modified button size, added partial arithmetic function

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var Overall=0.0, First=0.0, Second=0.0, Oper=null;
+
+function Clear() {
+  First=0.0;
+  Second=0.0;
+  Oper=null;
+  Overall=0.0;
+  //document.getElementById("output").InnerHTML = "0.0";
+}
+
+function Calculate(Overall) {
+	Overall = First;
+	if (Oper === "add") {
+		Overall = First + Second;
+	} else if (Oper === "subtract") {
+		Overall = First - Second;
+	} else if (Oper === "divide") {
+		Overall = First / Second;
+	} else if (Oper === "mult") {
+		Overall = First * Second;
+	}
+	return Overall;
+}
+
+// Shutup jshint
+Calculate(10);
+
+function Init() {
+  Clear();
+}
+
+Init();
+
+
